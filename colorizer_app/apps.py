fdf7c0e4c34d1f8c92b260a94a2868de0ab3f4b9
aidevabhi <abhishekdubey.connect@gmail.com>
2025-05-29 14:21:27 +0530

    Delete colorizer_app directory

--- colorizer_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class ColorizerAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'colorizer_app'
--- colorizer_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('upload/', views.upload_image, name='upload_image'),
-    path('result/<int:image_id>/', views.result, name='result'),
-    path('recolor/<int:image_id>/', views.recolor_image, name='recolor_image'),
-]
